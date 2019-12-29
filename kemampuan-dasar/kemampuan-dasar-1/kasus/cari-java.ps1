d311d1bae4bb6219b6a8f09499168a1b7bb3d8fd
Arie <arie1st_fx@yahoo.com>
2019-12-29 10:28:33 +0700

    initial commit

---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.ps1
@@ -0,0 +1,8 @@
+﻿$Dir = get-childitem F:\Courses\Praxis -recurse
+$List = $Dir | where {$_.extension -eq ".java"}
+#$List | format-table name
+$c=0;
+foreach ($file in $List){
+$c = $c+1;
+}
+Write-Host "ada" $c "file java"
---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,16 @@
+﻿$file = "F:\Courses\Praxis\nama-file.java"
+if(Test-Path $file){
+    $a = New-Object -ComObject Wscript.Shell 
+    $pop = $a.popup("nama-file.java ada, diganti namanya?",4,"Alert",4)
+    if($pop -eq 6){
+        $rename = Read-Host "masukan nama file baru"
+        Rename-Item -Path $file -NewName $rename
+        Write-Host " File dirubah menjadi" $rename
+    }
+    else{
+         Write-Host "OK! file tidak di rename"
+    }
+}
+else{
+Write-Host "File tidak ada"
+}
